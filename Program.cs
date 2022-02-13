6b0774a004232128080c0470c705d4d0e08cf143
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-13 20:48:20 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ifelseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = DateTime.Now.Hour ;
+            switch (hour)
+            {
+                case 13 :
+                case 14 :
+                case 15 :
+                case 16 :
+                Console.WriteLine("oglen zamanindasin!");
+                break;
+                case 7 :
+                case 8 :
+                case 9 :
+                case 10 :
+                case 11:
+                case 12:
+                Console.WriteLine("sabah zamanindasin!");
+                break;
+                case 19 :
+                case 20 :
+                case 21 :
+                case 22 :
+                case 23 :
+                case 24 :
+                Console.WriteLine("aksam zamanindasin!");
+                break;
+                default:
+                break;
+            }
+        }
+    }
+}
